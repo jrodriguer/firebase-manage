a75af61420020b11a4911ec291603c525d88ad94
jrodriguer <juliorodriguezramirez@gmail.com>
2023-10-19 09:25:39 +0200

    enable CORS on express

--- index.js
+++ index.js
@@ -30,6 +30,23 @@ app.set("view engine", "pug");
 app.use(bodyParser.json());
 app.use(urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With",
+  );
+  // allow preflight
+  if (req.method === "OPTIONS") {
+    res.send(200);
+  } else {
+    next();
+  }
+});
 
 app.get("/", (req, res) => res.render("index"));
 app.get("/translations", (req, res) => res.render("translations"));
