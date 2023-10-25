93d02cf35bf52d4b5e03432fa346ba44bb29a842
Julio <juliorodriguezramirez@gmail.com>
2023-10-25 12:59:08 +0200

    add posts routes method to send messages

--- src/routes/messaging.js
+++ src/routes/messaging.js
@@ -1,9 +1,11 @@
 var express = require("express")
 
-var messagingController = require("../controllers/messagings");
+var messagingController = require("../controllers/messagingController");
 
 var router = express.Router();
 
 router.get("/messaging", messagingController.messagingView);
+router.post("/send-message-topic", messagingController.sendMessageToTopic);
+router.post("/send-message-device", messagingController.sendMessageToDevice);
 
 module.exports = router;
