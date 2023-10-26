71e46b1cb4fa49666afcd4c9d24f49a2ef542007
Julio <juliorodriguezramirez@gmail.com>
2023-10-26 18:36:07 +0200

    update constructor and build message to send

--- src/models/FCMMessage.js
+++ src/models/FCMMessage.js
@@ -1,8 +1,26 @@
 class FCMMessage {
-  constructor(title, body, topic) {
+  constructor(token, title, body, topic = "all_users") {
+    this.token = token;
     this.title = title;
     this.body = body;
     this.topic = topic;
+  }
+
+  buildMessage() {
+    const message = {
+      notification: {
+        title: this.title,
+        body: this.body,
+      },
+    };
+
+    if (this.token != "") {
+      message.token = this.token;
+    } else {
+      message.topic = this.topic;
+    }
+
+    return message;
   }
 }
 
