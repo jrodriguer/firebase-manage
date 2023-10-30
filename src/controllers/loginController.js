3889e440734c6253b9c1f151cc0750b2513c9083
Julio <juliorodriguezramirez@gmail.com>
2023-10-30 12:54:38 +0100

    sign in for login view (wip)

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,10 +1,27 @@
-var admin = require("firebase-admin");
-var serviceAccount = require("../../placeholders/service-account.json");
-
-admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+const { auth } = require("../firebaseAdmin");
 
 const loginView = (req, res) => {
   res.render("login");
 };
 
-module.exports = loginView;
+const signIn = (req, res) => {
+  const { email, password } = req.body;
+
+  auth 
+    .signInWithEmailAndPassword(firebase.auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log({ user })
+
+        res.status(200).json({ message: "Login successfully" });
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log({ errorCode, errorMessage })
+
+        res.status(500).json({ error: "Login failed" });
+      });
+};
+
+module.exports = { loginView, signIn };
--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,11 +1,11 @@
-var express = require("express")
-
+var express = require("express");
 var messagingController = require("../controllers/messagingController");
 var loginController = require("../controllers/loginController");
 
 var router = express.Router();
 
-router.get("/login", loginController.loginView);
+router.get("/", loginController.loginView);
+router.post("/sign-in",  loginController.signIn);
 router.get("/messaging", messagingController.messagingView);
 router.post("/send-message", messagingController.sendMessage);
 
