8db1166324ed56da5f1739c99ca01fd4a750c10f
jrodriguer <juliorodriguezramirez@gmail.com>
2023-10-31 19:49:47 +0100

    refactor properties vars and add export

--- src/utils/properties.js
+++ src/utils/properties.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var BASE_URI_DES = "https://backend-des.wenea.site/api",
   BASE_URI_PRE = "https://backend-pre.wenea.site/api",
   BASE_URI_PRO = "https://backend-pro.wenea.site/api",
@@ -6,11 +8,19 @@ var BASE_URI_DES = "https://backend-des.wenea.site/api",
 var WENEA_VERSION = "3.0.2";
 
 // Endpoint configuration
-var BASE_URI = BASE_URI_DES;
-
-var USER_ENDPOINT = BASE_URI + "/" + WENEA_API_VERSION + "/user";
+var BASE_URI = BASE_URI_DES,
+  USER_ENDPOINT = BASE_URI + "/" + WENEA_API_VERSION + "/user";
 
 // User-related endpoints
 var WENEA_USER_LOGIN = USER_ENDPOINT + "/login/";
 
-// TODO: Export file
+module.exports = {
+  BASE_URI_DES: BASE_URI_DES,
+  BASE_URI_PRE: BASE_URI_PRE,
+  BASE_URI_PRO: BASE_URI_PRO,
+  BASE_URI_DEHESA: BASE_URI_DEHESA,
+  WENEA_VERSION: WENEA_VERSION,
+  BASE_URI: BASE_URI,
+  USER_ENDPOINT: USER_ENDPOINT,
+  WENEA_USER_LOGIN: WENEA_USER_LOGIN,
+};
