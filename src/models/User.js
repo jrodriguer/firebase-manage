57dec07b5a7ba60857debc5441520fc7e7689ce4
Julio <juliorodriguezramirez@gmail.com>
2023-11-07 10:56:37 +0100

    user model (wip)

---
+++ src/models/User.js
@@ -0,0 +1,11 @@
+/**
+ * User class.
+ *
+ * @constructor
+ * @param {String} email - title message to send.
+ * @param {String} password - body message to send.
+ */
+function User(email, password) {
+  this.email = email
+  this.password = password
+}
