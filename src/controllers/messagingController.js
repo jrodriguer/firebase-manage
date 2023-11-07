7da3f2ed0942196b6bb8ec32ad33578e7921acf7
Julio <juliorodriguezramirez@gmail.com>
2023-11-07 10:59:15 +0100

    sytle sintax refactor

--- src/controllers/messagingController.js
+++ src/controllers/messagingController.js
@@ -1,30 +1,30 @@
-var admin = require("../firebaseAdmin"),
-  FCMMessage = require("../models/FCMMessage");
+var admin = require('../firebaseAdmin'),
+  FCMMessage = require('../models/FCMMessage')
 
 function messagingView(req, res) {
-  res.render("messaging");
+  res.render('messaging')
 }
 
 function sendMessage(req, res) {
   var token = req.body.token,
     title = req.body.title,
-    body = req.body.body;
-  var fcmMessage = FCMMessage.call(token, title, body);
-  var message = fcmMessage.buildMessage();
+    body = req.body.body
+  var fcmMessage = FCMMessage.call(token, title, body)
+  var message = fcmMessage.buildMessage()
 
   admin
     .messaging()
     .send(message)
     .then(function (response) {
-      console.log("Successfully sent message to topic:" + response);
-      res.status(200).json({ message: "Message sent successfully" });
+      console.log('Successfully sent message to topic:' + response)
+      res.status(200).json({ message: 'Message sent successfully' })
     })
     .catch(function (error) {
-      res.status(500).json({ error: "Failed to send message" });
-    });
+      res.status(500).json({ error: 'Failed to send message' })
+    })
 }
 
 module.exports = {
   messagingView: messagingView,
   sendMessage: sendMessage,
-};
+}
--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,12 +1,12 @@
-var express = require("express"),
-  messagingController = require("../controllers/messagingController"),
-  loginController = require("../controllers/loginController");
+var express = require('express'),
+  messagingController = require('../controllers/messagingController'),
+  loginController = require('../controllers/loginController')
 
-var router = express.Router();
+var router = express.Router()
 
-router.get("/", loginController.loginView);
-router.post("/login", loginController.login);
-router.get("/messaging", messagingController.messagingView);
-router.post("/send-message", messagingController.sendMessage);
+router.get('/', loginController.loginView)
+router.post('/login', loginController.login)
+router.get('/messaging', messagingController.messagingView)
+router.post('/send-message', messagingController.sendMessage)
 
-module.exports = router;
+module.exports = router
