d31e4264c46ad921f44a2e884dc32b70e6a3605c
Julio <juliorodriguezramirez@gmail.com>
2023-11-13 18:09:09 +0100

    add translations view route

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,6 +1,7 @@
 var express = require( "express" ),
   messagingController = require( "../controllers/messagingController" ),
-  loginController = require( "../controllers/loginController" );
+  loginController = require( "../controllers/loginController" ),
+  translationController = require( "../controllers/translationController" );
 
 var router = express.Router();
 
@@ -8,5 +9,6 @@ router.get( "/", loginController.loginView );
 router.post( "/login", loginController.login );
 router.get( "/messaging", messagingController.messagingView );
 router.post( "/send-message", messagingController.sendMessage );
+router.post( "/translations", translationController.translationView );
 
 module.exports = router;
