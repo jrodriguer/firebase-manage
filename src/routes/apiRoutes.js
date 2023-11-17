ad8da0e841a99a5af8f09cb08e618d4232456de6
Julio <juliorodriguezramirez@gmail.com>
2023-11-17 04:32:17 +0100

    wip

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -15,6 +15,6 @@ router.get( "/translation", translationController.translationView );
 router.get( "/download-template", translationController.downloadTemplate );
 router.get( "/list-versions", translationController.listVersions );
 router.post( "/publish-template", translationController.publishTemplate );
-router.post( "/update-template", translationController.getAndUpdateTemplate );
+router.put( "/update-template", translationController.getAndUpdateTemplate );
 
 module.exports = router;
