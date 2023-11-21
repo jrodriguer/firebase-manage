32f8f9a28ccae2961f3620af30bded82c78ba53e
Julio <juliorodriguezramirez@gmail.com>
2023-11-21 16:46:21 +0100

    send status 200

--- src/controllers/translationController.js
+++ src/controllers/translationController.js
@@ -1,11 +1,12 @@
-var admin = require( "firebase-admin" ),
-  multer = require( "multer" ),
-  fs = require( "fs" ),
-  https = require( "https" );
+const admin = require( "firebase-admin" );
+const fs = require( "fs" );
+const https = require( "https" );
+const multer = require( "multer" );
+const util = require( "util" );
+const readFile = util.promisify( fs.readFile );
 
-var config = admin.remoteConfig();
-
-var upload = multer({ 
+const config = admin.remoteConfig();
+const upload = multer({ 
   dest: "uploads/" 
 });
 
@@ -13,151 +14,124 @@ function translationView( req, res ) {
   res.render( "translation" );
 }
 
-function downloadTemplate( req, res, next ) {
-  admin
-    .remoteConfig()
-    .getTemplate()
-    .then( function( template ) {
-      console.log( "ETag from server: " + template.etag );
-      res.send( JSON.stringify( template ));
-    })
-    .catch( function( err ) {
-      next( err );
-    });
-}
-
 function listVersions( req, res, next ) {
   admin
     .remoteConfig()
     .listVersions()
-    .then( function( listVersionsResult ) {
-      console.log( "Successfully fetched the list of versions" );
-
+    .then(( listVersionsResult ) => {
       var versions = [];
       listVersionsResult.versions.forEach( function( version ) {
         versions.push( version );
       });
-
       res.send( versions );
-    })
-    .catch( function( err ) {
-      next( err );
-    });
+    }, next );
+}
+
+function downloadTemplate( req, res, next ) {
+  admin
+    .remoteConfig()
+    .getTemplate()
+    .then(( template ) => {
+      console.log( "ETag from server: " + template.etag );
+      res.send( template );
+      res.sendStatus( 200 );
+    }, next );
 }
 
 function _validateTemplate( template ) {
-  return new Promise( function( resolve, reject ) {
-    config
+  return new Promise(( resolve, reject ) => {
+    config 
       .validateTemplate( template )
-      .then( function() {
-        console.log( "Template was valid and safe to use" );
+      .then(() => {
         resolve( true );
       })
-      .catch( function( err ) {
-        console.error( "Template is invalid and cannot be published" );
-        console.error( err );
+      .catch(( err ) => {
         resolve( false );
       });
   });
 }
 
-function getAndUpdateTemplate( req, res, next ) {
-  var { name, 
+async function getAndUpdateTemplate( req, res, next ) {
+  let { name, 
     expression,
     parameter,
     defaultValue,
     conditionalValue } = req.body;
 
-  console.log( req.body );
-
   try {
-    config.getTemplate()
-      .then( function( template ) {
-        template.conditions.push({
-          name: name, 
-          expression: expression,
-          tagColor: "BLUE"
-        });
-        template.parameters[parameter] = { 
-          defaultValue: { 
-            value: defaultValue 
-          },
-          conditionalValues: { 
-            [name]: { 
-              value: conditionalValue 
-            } 
-          } 
-        };
+    let template = await config.getTemplate();
+    template.conditions.push({
+      name: name, 
+      expression: expression,
+      tagColor: "BLUE"
+    });
+    template.parameters[parameter] = { 
+      defaultValue: { 
+        value: defaultValue 
+      },
+      conditionalValues: { 
+        [name]: { 
+          value: conditionalValue 
+        } 
+      } 
+    };
 
-        return _validateTemplate( template )
-          .then( function( isValid ) {
-            if ( !isValid ) {
-              throw new Error( "Template is invalid" );
-            }
-            return config.publishTemplate( template );
-          });
-      })
-      .then( function( updatedTemplate ) {
-        console.log( "ETag from server: " + updatedTemplate.etag );
-        res.status( 200 ).send( "Template update published" );
-      })
-      .catch( function( err ) {
-        console.error( "Failed to publish the update template: ", err );
-        next( err );
-      });
+    let isValid = await _validateTemplate( template );
+    if ( !isValid ) {
+      // TODO: We define a custom BadRequestError class that extends Error to represent the 400 Bad Request error.
+      throw new Error( "Template is invalid." );
+    }
+
+    let updatedTemplate = await config.publishTemplate( template );
+    res.status( 200 ).send( "Template updated." );
+    console.log( "ETag from server: " + updatedTemplate.etag );
   }
   catch ( err ) {
-    console.error( "Unable to get and update template." );
-    return next( new Error( err ));
+    console.error( "Unable update template." );
+    return next( err );
   }
 }
 
-function publishTemplate( req, res, next ) {
-  var template;
-
+async function publishTemplate( req, res, next ) {
   try {
-    upload.single( "publish" )( req, res, function( err ) {
-      if ( err ) {
-        return next( new Error( "Error uploading file" ));
-      }
-
-      if ( !req.file ) {
-        throw new Error( "No file uploaded" );
-      }
-
-      var fileContent = fs.readFileSync( req.file.path, "UTF8" );
-      template = config.createTemplateFromJSON( fileContent );
-
-      var ifMatch = template.etag;
-
-      _validateTemplate( template )
-        .then( function( isValid ) {
-          if ( !isValid ) {
-            throw new Error( "Template is invalid" );
-          }
-          _sendPutRequest( template, ifMatch )
-            .then( function( response ) {
-              return _toRemoteConfigTemplate( response );
-            });
-          // return config.publishTemplate( template );
-        })
-        .then( function( publication ) {
-          console.log( "Template has been published" );
-          console.log( "ETag from server: " + publication.etag );
-          res.status( 200 ).send( "Template has been published" );
-        })
-        .catch( function( err ) {
-          console.error( "Failed to publish template: ", err );
-          next( err );
-        });
+    await new Promise(( resolve, reject ) => {
+      upload.single( "publish" )( req, res, ( err ) => {
+        if ( err ) {
+          return reject( new Error( "Error uploading file." ));
+        }
+        if ( !req.file ) {
+          throw new Error( "No file uploaded." );
+        }
+        resolve();
+      });
     });
+
+    const fileContent = await readFile( req.file.path, "UTF8" );
+    let template = config.createTemplateFromJSON( fileContent );
+
+    // TODO: Bad validation, udpate for "step to step" validator. 
+    let isValid = await _validateTemplate( template );
+    if ( !isValid ) {
+      // TODO: We define a custom BadRequestError class that extends Error to represent the 400 Bad Request error.
+      throw new Error( "Template is invalid." );
+    }
+
+    let publishedTemplate = await config.publishTemplate( template );
+    res.status( 200 ).send( "Template published." );
+    console.log( "ETag from server: " + publishedTemplate.etag );
   } 
   catch ( err ) {
-    return next( new Error( err ));
+    console.error( "Unable to publish template." );
+    next( err );
   }
 }
 
-function _sendPutRequest( template, etag ) {
+function _sendPutRequest( template, etag, validateOnly ) {
+  var path = "remoteConfig";
+  if ( validateOnly ) {
+    path += "?validate_only=true";
+  }
+
   var data = {
     conditions: template.conditions,
     parameters: template.parameters,
@@ -168,68 +142,47 @@ function _sendPutRequest( template, etag ) {
   var options = {
     hostname: "firebaseremoteconfig.googleapis.com",
     port: 443,
-    path: "/v1/projects/flutter-news-app-6a808/remoteConfig",
-    method: "PUT",
+    path: "/v1/projects/flutter-news-app-6a808/" + path,
+    method: "POST",
     headers: { 
+      "Content-Type": "application/json; UTF8",
       "X-Firebase-Client": "firebase-manage/1.0.0",
       "Accept-Encoding": "gzip",
       "If-Match": etag
     }
   };
 
-  var request = https.request( options, function( res ) {
-    var responseBody = "";
-    res.setEncoding( "utf8" );
-
-    res.on( "data", function( chunk ) {
-      console.log( "Chunk data firebase remote config" );
-      responseBody += chunk;
-    });
-
-    res.on( "end", function() {
-      if ( res.statusCode >= 200 && res.statusCode < 300 ) {
-        try {
-          var parsedResponse = JSON.parse( responseBody );
-          res.status( res.statusCode ).json( parsedResponse );
-        }
-        catch ( err ) {
-          res.status( 500 ).json({ 
-            error: "Failed to parse JSON res" 
-          });
-        }
-      } 
-      else {
-        res.status( res.statusCode ).json({ 
-          error: "Request failed" 
-        });
+  return new Promise(( resolve, reject ) => {
+    var req = https.request( options, ( res ) => {
+      if ( res.statusCode < 200 || res.statusCode > 299 ) {
+        return reject( new Error( "HTTP status code " + res.statusCode ));
       }
+
+      const body = [];
+
+      res.on( "data", ( chunk ) => {
+        console.log( "Chunk data firebase remote config" );
+        body.push( chunk );
+      });
+
+      res.on( "end", () => {
+        const resString = Buffer.concat( body ).toString();
+        resolve( resString );
+      });
     });
+
+    req.on( "error", ( err ) => {
+      reject( err );
+    });
+
+    req.on( "timeout", () => {
+      req.destroy();
+      reject( new Error( "req time out" ));
+    });
+
+    req.write( JSON.stringify( data ));
+    req.end(); 
   });
-
-  request.on( "error", function( err ) {
-    console.error( err );
-  });
-
-  request.write( data );
-  request.end(); 
-}
-
-function _toRemoteConfigTemplate( resp ) {
-  var etag = resp.headers["etag"];
-  _validateEtag( etag );
-  return {
-    conditions: resp.data.conditions,
-    parameters: resp.data.parameters,
-    parameterGroups: resp.data.parameterGroups,
-    etag,
-    version: resp.data.version
-  };
-}
-
-function _validateEtag( etag ) {
-  if ( etag != "" || etag != undefined ) {
-    throw new Error( "ETag header is not present in the server response." );
-  }
 }
 
 module.exports = { 
