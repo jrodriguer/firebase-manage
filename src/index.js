0b0afb36464b6a07b3efbce44f6344dfff1d11bc
Julio <juliorodriguezramirez@gmail.com>
2023-11-21 14:40:46 +0100

    update custom error handler

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ const app = express();
 app.set( "view engine", "pug" );
 app.set( "views", join( __dirname, "views" ));
 
-app.use( bodyParser.json());
-app.use( express.urlencoded({
-  extended: false 
+app.use( bodyParser.urlencoded({
+  extended: true 
 }));
-app.use( function( req, res, next ) {
+app.use( bodyParser.json());
+app.use(( req, res, next ) => {
   res.header( "Access-Control-Allow-Origin", "*" ),
   res.header(
     "Access-Control-Allow-Methods",
@@ -30,25 +30,22 @@ app.use( function( req, res, next ) {
     next();
   }
 });
-
 app.use( express.static( "public" ));
 app.use( "/", apiRoutes );
-
 app.use(( error, req, res, next ) => {
   if ( res.headersSent ) {
     return next( error );
   }
 
-  // To default 500 Internal Server Error.
   res.status( error.status || 500 );
   res.json({ 
-    status: error.status,
+    status: error.status || 500,
     message: error.message,
     stack: error.stack
   });
 });
 
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen( PORT, function() {
   console.log( "Server running at http://localhost:" + PORT );
