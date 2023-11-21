9866a625f7497ebed8d67c545454a45f4b4e4204
Julio <juliorodriguezramirez@gmail.com>
2023-11-21 14:40:18 +0100

    to es6 class

--- src/models/FCMMessage.js
+++ src/models/FCMMessage.js
@@ -1,54 +1,26 @@
-/**
- * FCMMessage class.
- *
- * @constructor
- * @param {String} token - personal device token.
- * @param {String} title - title message to send.
- * @param {String} body - body message to send.
- * @param {String} topic - topic suscribe.
- */
-function FCMMessage( token, title, body, topic ) {
-  this.token = token;
-  this.title = title;
-  this.body = body;
-  this.topic = "all_users";
-}
-
-function buildMessage() {
-  const message = {
-    notification: {
-      title: this.title,
-      body: this.body
-    }
-  };
-
-  if ( !this.token ) {
-    message.token = this.token;
-  } 
-  else {
-    message.topic = this.topic;
+export class FCMMessage {
+  constructor( token, title, body, topic ) {
+    this.token = token;
+    this.title = title;
+    this.body = body;
+    this.topic = "all_users";
   }
 
-  return message;
+  buildMessage() {
+    const message = {
+      notification: {
+        title: this.title,
+        body: this.body
+      }
+    };
+
+    if ( !this.token ) {
+      message.token = this.token;
+    } 
+    else {
+      message.topic = this.topic;
+    }
+
+    return message;
+  }
 }
-
-FCMMessage.prototype.getToken = function() {
-  return this.token;
-};
-
-FCMMessage.prototype.getTitle = function() {
-  return this.title;
-};
-
-FCMMessage.prototype.getBody = function() {
-  return this.body;
-};
-
-FCMMessage.prototype.getTopic = function() {
-  return this.topic;
-};
-
-module.exports = {
-  FCMMessage: FCMMessage, 
-  buildMessage: buildMessage
-};
