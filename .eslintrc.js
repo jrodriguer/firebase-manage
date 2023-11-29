7ab7a2b6896f60b410cfecea013801aed78f5206
Julio <juliorodriguezramirez@gmail.com>
2023-11-29 14:18:12 +0100

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,114 +1,203 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "node": true
+  env: {
+    browser: true,
+    node: true
   },
-  "extends": [ "eslint:recommended" ],
-  "globals": { 
-    "Atomics": "readonly", 
-    "SharedArrayBuffer": "readonly" 
+  extends: [
+    "eslint:recommended"
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
   },
-  "parser": "@babel/eslint-parser",
-  "parserOptions": { 
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
-  "rules": {
-    "array-bracket-newline": [ "error", "consistent" ],
-    "array-bracket-spacing": [ "error", "always", { 
-      "singleValue": true, 
-      "objectsInArrays": false,
-      "arraysInArrays": false
-    }],
-    "arrow-parens": [ "error", "always" ],
-    "brace-style": [ "error", "stroustrup" ],
-    "comma-dangle": [ "error", "never" ],
-    "comma-spacing": [ "error", { 
-      "before": false, 
-      "after": true 
-    }],
-    "computed-property-spacing": [ "error", "never" ],
-    "curly": [ "error" ],
-    "dot-location": [ "error", "property" ],
-    "eol-last": [ "error", "always" ],
-    "indent": [ "error", 2 ],
-    "linebreak-style": [ "error", "unix" ],
-    "max-len": [ "error", { 
-      "code": 100 
-    }],
-    "multiline-ternary": [ "error", "always-multiline" ],
-    "nonblock-statement-body-position": [ "error", "below" ],
-    "no-multiple-empty-lines": [ "error", {
-      "max": 1 
-    }],
+  rules: {
+    "array-bracket-newline": [
+      "error",
+      "consistent"
+    ],
+    "array-bracket-spacing": [
+      "error",
+      "always",
+      {
+        arraysInArrays: false,
+        objectsInArrays: false,
+        singleValue: true
+      }
+    ],
+    "arrow-parens": [
+      "error",
+      "always"
+    ],
+    "brace-style": [
+      "error",
+      "stroustrup"
+    ],
+    "comma-dangle": [
+      "error",
+      "never"
+    ],
+    "comma-spacing": [
+      "error",
+      {
+        after: true,
+        before: false
+      }
+    ],
+    "computed-property-spacing": [
+      "error",
+      "never"
+    ],
+    curly: [
+      "error"
+    ],
+    "dot-location": [
+      "error",
+      "property"
+    ],
+    "eol-last": [
+      "error",
+      "always"
+    ],
+    indent: [
+      "error",
+      2
+    ],
+    "key-spacing": [
+      "error",
+      {
+        mode: "strict"
+      }
+    ],
+    "keyword-spacing": [
+      "error",
+      {
+        after: false,
+        overrides: {
+          catch: {
+            after: true
+          },
+          const: {
+            after: true
+          },
+          constructor: {
+            after: true
+          },
+          else: {
+            after: true
+          },
+          from: {
+            after: true
+          },
+          if: {
+            after: true
+          },
+          import: {
+            after: true
+          },
+          return: {
+            after: true
+          },
+          throw: {
+            after: true
+          },
+          try: {
+            after: true
+          }
+        }
+      }
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "max-len": [
+      "error",
+      {
+        code: 100
+      }
+    ],
+    "multiline-ternary": [
+      "error",
+      "always-multiline"
+    ],
     "no-multi-spaces": "error",
-    "no-unused-vars": [ 
-      "error", 
-      { 
-        "vars": "all", 
-        "args": "after-used", 
-        "ignoreRestSiblings": false 
-      } 
+    "no-multiple-empty-lines": [
+      "error",
+      {
+        max: 1
+      }
+    ],
+    "no-unused-vars": [
+      "error",
+      {
+        args: "after-used",
+        ignoreRestSiblings: false,
+        vars: "all"
+      }
     ],
     "no-whitespace-before-property": "error",
-    "object-curly-spacing": [ "error", "always", { 
-      "objectsInObjects": true 
-    }],
-    "object-curly-newline": [ "error", { 
-      "ObjectExpression": "always", "ObjectPattern": "never" 
-    }],
-    "padded-blocks": [ "error", "never" ],
-    "keyword-spacing": [ "error", { 
-      after: false, "overrides": { 
-        "return": { 
-          "after": true 
-        }, 
-        "var": { 
-          "after": true 
-        }, 
-        "if": { 
-          "after": true 
-        }, 
-        "else": { 
-          "after": true 
-        }, 
-        "throw": { 
-          "after": true 
-        }, 
-        "try": { 
-          "after": true 
-        }, 
-        "catch": { 
-          "after": true 
-        }, 
-        "case": { 
-          "after": true 
-        },
-        "while": { 
-          "after": false 
-        },
-        "static": { 
-          "after": false 
-        },
-        "as": { 
-          "after": false 
-        }
-      } 
-    }],
+    "nonblock-statement-body-position": [
+      "error",
+      "below"
+    ],
+    "object-curly-newline": [
+      "error",
+      {
+        ObjectExpression: "always",
+        ObjectPattern: "never"
+      }
+    ],
+    "object-curly-spacing": [
+      "error",
+      "always",
+      {
+        objectsInObjects: true
+      }
+    ],
+    "padded-blocks": [
+      "error",
+      "never"
+    ],
+    quotes: [
+      "error",
+      "double"
+    ],
     radix: "off",
-    "semi": [ "error", "always" ],
-    "space-before-function-paren": [ "error", "never" ],
-    "space-in-parens": [ "error", "always", { 
-      "exceptions": [ "empty", "{}", "()" ] 
-    }],
+    semi: [
+      "error",
+      "always"
+    ],
+    "space-before-function-paren": [
+      "error", {
+        "anonymous": "always",
+        "named": "always",
+        "asyncArrow": "always"
+      }
+    ],
+    "space-in-parens": [
+      "error",
+      "always",
+      {
+        exceptions: [
+          "empty",
+          "{}",
+          "()"
+        ]
+      }
+    ],
     "space-infix-ops": "error",
-    "switch-colon-spacing": [ "error", { 
-      "after": true, "before": false 
-    }],
-    "quotes": [ "error", "double" ],
-    "key-spacing": [ "error", { 
-      "mode": "strict" 
-    }],
-    yoda: "error",
+    "switch-colon-spacing": [
+      "error",
+      {
+        after: true,
+        before: false
+      }
+    ],
+    yoda: "error"
   }
 };
